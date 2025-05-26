d6478d7e8424d425716c7932ca7f65d93e92e299
jithendrajasti <jithendrajasti11@gmail.com>
2025-05-27 02:01:20 +0530

    Add files via upload
    files uploaded

---
+++ script.js
@@ -0,0 +1,21 @@
+const flip = document.querySelector('button');
+const coin = document.querySelector('img');
+const result=document.getElementById('result');
+const sound=new Audio('/cointosser/coinflip.mp3');
+const flipper = function () {
+    sound.play();
+    coin.style.transition = 'transform 1s';
+    coin.style.transform = 'rotateY(1200deg)'; 
+    setTimeout(() => {
+        const isHeads = Math.floor(Math.random()*2);
+        coin.src = isHeads ? '/cointosser/headssvg.svg' : '/cointosser/tailssvg.svg';
+        if(isHeads)
+            result.textContent="head";
+        else
+            result.textContent="tail";
+        coin.style.opacity = 1;
+        coin.style.transform = 'rotateY(0deg)';
+    }, 1000);
+};
+
+flip.addEventListener('click', flipper);
