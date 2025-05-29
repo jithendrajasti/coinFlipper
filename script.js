af53c9e3b9679945b75c0f4ea9fc53343ea0e287
jithendrajasti <jithendrajasti11@gmail.com>
2025-05-29 19:28:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const flip = document.querySelector('button');
 const coin = document.querySelector('img');
 const result=document.getElementById('result');
-const sound=new Audio('/coinflip.mp3');
+const sound=new Audio('coinflip.mp3');
 const flipper = function () {
     sound.play();
-    coin.style.transition = 'transform 1s';
+    coin.style.transition = 'transform 2s';
     coin.style.transform = 'rotateY(9000deg)'; 
     setTimeout(() => {
         const isHeads = Math.floor(Math.random()*2);
-        coin.src = isHeads ? '/headssvg.svg' : '/tailssvg.svg';
+        coin.src = isHeads ? 'headssvg.svg' : 'tailssvg.svg';
         coin.style.opacity = 1;
         coin.style.transform = 'rotateY(0deg)';
         setTimeout(()=>{
