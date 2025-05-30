c1cfcd3b011689ca455589a186144d0e7e6b6bd5
jithendrajasti <jithendrajasti11@gmail.com>
2025-05-30 09:38:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const flip = document.querySelector('button');
 const coin = document.querySelector('img');
 const result=document.getElementById('result');
 const sound=new Audio('coinflip.mp3');
+  let flag;
 const flipper = function () {
+    if(flag==true)
+        return;
+    flag=true;
     sound.play();
     coin.style.transition = 'transform 2s';
     coin.style.transform = 'rotateY(9000deg)'; 
@@ -19,6 +23,7 @@ const flipper = function () {
             result.textContent="tail";
        },1000);
     }, 1000);
+     flag=false;
 };
 
 flip.addEventListener('click', flipper);
